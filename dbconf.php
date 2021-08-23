b028695a5396ae4ac10af77973a3769e2ac2d8c4
Nokline <75624264+Nokline@users.noreply.github.com>
2021-08-23 22:12:31 +0300

    Update dbconf.php

--- dbconf.php
+++ dbconf.php
@@ -1,1 +1,17 @@
+<?php
+
+$dbserver = "";
+$username = "";
+$password = "";
+$dbname = "";
+
+
+$connect = mysqli_connect($dbserver, $username, $password, $dbname);
+
+if(!$connect){
+	die("Connection failed: ".$connect->connect_error);
+}
+
+
+?>
 
